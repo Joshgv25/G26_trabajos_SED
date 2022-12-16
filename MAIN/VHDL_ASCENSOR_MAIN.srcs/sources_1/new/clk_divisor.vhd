4081e3265de344d460345f787c44ccface2da1b3
Joshgv25 <61049984+Joshgv25@users.noreply.github.com>
2022-12-16 13:37:08 +0100

    Modificacion animacion y divisor de frecuencia
    Creacion del divisor de frecuencia con la que obtendremos una señal derivada del reloj util para algunos componentes. Modificacion de la implementacion de la animacion, que se hará mediante una maquina de estados

---
+++ MAIN/VHDL_ASCENSOR_MAIN.srcs/sources_1/new/clk_divisor.vhd
@@ -0,0 +1,60 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 16.12.2022 13:17:31
+-- Design Name: 
+-- Module Name: clk_divisor - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity clk_divisor is
+ Generic (frec: integer:=50000000);  -- default value is for 2hz
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           clk_out : out  STD_LOGIC);
+end clk_divisor;
+
+architecture Behavioral of clk_divisor is
+signal clk_sig: std_logic;
+begin
+
+  process (clk,reset)
+  variable cnt:integer;
+  begin
+		if (reset='1') then
+		  cnt:=0;
+		  clk_sig<='0';
+		elsif clk'event and clk='1' then
+			cnt:=cnt+1;
+            if (cnt=frec) then
+				cnt:=0;
+				clk_sig<=not(clk_sig);
+			end if;
+		end if;
+  end process;
+  clk_out<=clk_sig;
+end Behavioral;
