804a5e05fb777669845df59f5cf1e70ea0854d94
Eloy <e.isidro@alumnos.upm.es>
2022-12-23 18:24:20 +0100

    Correcciones en el fsm
    Cambios y correcciones del fsm

--- MAIN/VHDL_ASCENSOR_MAIN.srcs/sources_1/new/FSM_ascensor.vhd
+++ MAIN/VHDL_ASCENSOR_MAIN.srcs/sources_1/new/FSM_ascensor.vhd
@@ -17,8 +17,6 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -36,8 +34,8 @@ port(
     		--entradas
 			clk :   in  std_logic; --reloj
    			reset_n : in std_logic;  --vuelve al piso 0 siempre
-   			pAct:   in std_logic_vector(3 downto 0); --piso actual
-        	pCall:  in std_logic_vector(3 downto 0); --piso del que llaman (salida del filtro)
+   			pAct:   in std_logic_vector(3 downto 0); --piso actual (salida del filtro) (switches)
+        	pCall:  in std_logic_vector(3 downto 0); --piso al que queremos ir (botones) 
    			filtro: in std_logic; --si llega un 1, tiene en cuenta a pAct
    			rearme: in std_logic;
      		--salidas
@@ -50,12 +48,12 @@ architecture Behavioral of FSM_ascensor is
 type estados is(s0, s1, s2, s3, s4); --s0 reposo, s1 subir, s2 bajar, s3 emergencia (ir al 0), s4 averia
     signal current_state: estados;       --estado actual
     signal next_state: estados;          --estado siguiente
-    signal vector_switch: std_logic_vector(3 downto 0); 
+    signal vector_switch: std_logic_vector(3 downto 0); --
 begin 
-      process(filtro)
+      process(filtro)--------------------------------------------------------------------------------------
       begin 
       		 if filtro = '1' then 
-        	 	 vector_switch <= pCall;
+        	 	 vector_switch <= pAct;--le asignamos al vector_switch la entrada pAct
         	 end if; 
       end process;
  
@@ -77,19 +75,19 @@ begin
                    case current_state is
                     	when s0 =>
                            
-                           if pAct>vector_switch then
+                           if pCall>vector_switch then
                            		next_state <= s1; --si piso actual mayor que desde el que llamo, subira
-                           elsif pAct=vector_switch then
+                           elsif pCall=vector_switch then
                            		next_state <= s0; --si llamo desde el piso actual no me muevo
-                           else
-                           		next_state <= s2; --
+                           else --si el piso al que hay que ir está por deajo del que estoy
+                           		next_state <= s2;--me iré al estado de bajada
                            end if;
                                
                         when s1 =>
                         	
-                            if(pAct>vector_switch) then
-                           		next_state <= s1; --sigo subiendo porque el piso en que estoy esta mas arriba
-                            elsif(pAct=vector_switch) then
+                            if(pCall>vector_switch) then
+                           		next_state <= s1; --sigo subiendo porque el piso dnd quiero ir está más arriba
+                            elsif(pCall=vector_switch) then--cuando llego al piso dnd quería ir
                            		next_state <= s0; --dejo de subir y entro en reposo
                             else
                            		next_state <= s4; --entro en averia, no puedo bajar mientras estoy subiendo
@@ -97,31 +95,33 @@ begin
                             
                         when s2 =>
                         
-                           	if pAct>vector_switch then
+                           	if pCall>vector_switch then
                            		next_state <= s4; --averia, no puedo subir mientras bajo
-                            elsif pAct=vector_switch then
+                            elsif pAct=vector_switch then--cuando llego al piso dnd quería ir
                            		next_state <= s0; --paro de bajar y entro en reposo
                             else
                            		next_state <= s2; --sigo bajando, el piso al que quiero ir está más abajo
                            end if;     
                            
-               			when s3 =>
-                        
-                            if pAct>vector_switch then --ESTE ESTADO ES IMPOSIBLE
-                           		next_state <= s4; --no puedo bajar, ya estoy en el cero. paso a la averia
-                            elsif pAct=vector_switch then
-                           		next_state <= s0; --me mantengo en reposo en el 0
-                            else
-                           		next_state <= s1; --subo si se pulsa un boton que no sea el cero
+               			when s3 => --estando en el estado de reset o saiendo de emergencia (ir al piso 0)
+                           --if pAct>vector_switch then --ESTE ESTADO ES IMPOSIBLE
+                           		--next_state <= s4; --no puedo bajar, ya estoy en el cero. paso a la averia
+                           --elsif pAct=vector_switch then
+                           		--next_state <= s0; --me mantengo en reposo en el 0
+                            --else
+                           		--next_state <= s1; --subo si se pulsa un boton que no sea el cero
+                           --end if;
+                           if vector_switch = "0001" then --cuando hemos llegado al piso 0
+                                next_state <= S0; --pasamos al estado de reposo
                            end if;
                            
                         when s4 =>
                         
                         	if rearme='1' then
                            		 next_state <= S3; --si rearmo paso al piso 0 en estado de emergencica
-                            else 
-                            	 next_state <= s4; --si no rearmo seguire en averia
-                            end if;  
+                            else -------------------------------------------------------------------------------------
+                            	 next_state <= s4; --si no rearmo seguire en averia-----------------------------------
+                            end if;  ---------------------------------------------------------------------------------
                             
                         end case;
                   end if;
@@ -139,14 +139,13 @@ begin
                         when s2 =>
                         	motor <= "10"; --motor encendido y bajando
                             puerta <= '0'; --puerta cerrada
-                        when s3 =>
+                        when s3 =>--bajando al piso 0
                         	motor <= "10"; --motor encendido, bajo al 0
-                            puerta <= '1'; --puerta abierta
+                            puerta <= '0'; --puerta cerrada
                         when s4 =>
                         	motor <= "00"; --motor apagado, no sube ni baja
                             puerta <= '0'; --puerta cerrada por seguridad
       			end case;
       end process;
-
 
 end Behavioral;
