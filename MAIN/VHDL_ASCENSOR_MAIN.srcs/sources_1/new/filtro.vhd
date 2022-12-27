ff1b6e24fb6e46ae7b021cb088766ab00dcf9577
Eloy <e.isidro@alumnos.upm.es>
2022-12-27 21:52:48 +0100

    mix_vector
    Incluir componente mix_vector y arreglar filtro

--- MAIN/VHDL_ASCENSOR_MAIN.srcs/sources_1/new/filtro.vhd
+++ MAIN/VHDL_ASCENSOR_MAIN.srcs/sources_1/new/filtro.vhd
@@ -31,6 +31,7 @@ entity filtro is
     port(
         motor: in std_logic_vector(1 downto 0); 
         switch_bit: in std_logic_vector(3 downto 0);
+        clk: in std_logic;
         validez : out std_logic;
         sig_salida: out std_logic_vector(3 downto 0)
     );
@@ -41,26 +42,28 @@ architecture Behavioral of filtro is
     signal aux_sube: std_logic_vector(3 downto 0);
     signal aux_baja: std_logic_vector(3 downto 0);
 begin
-    edge: process(switch_bit)
+    edge: process(switch_bit, clk, motor)
     begin
         --comparacion de la señal switch anterior y la posicion
-        if motor(1) = '1' then --si el motor está activo
-            if motor(0) = '1' then --si está subiendo
-                --comparar posicion(entrada) y switch_ant(signal de la arquitectura)
-                if switch_bit /= aux_sube then --si la posicion no es la que deberia ser 
-                    validez<= '0'; --hay fallo
-                else
-                    validez <= '1'; --no hay ningun fallo
-                end if;
-            else --sin motor es 0 (está bajando)
-                if switch_bit /= aux_baja then
-                    validez <= '0'; --sale error
-                else
-                    validez <= '1'; --no hay ningún fallo
-                end if;
+        if rising_edge(clk) then
+            if motor(1) = '1' then --si el motor está activo
+                if motor(0) = '1' then --si está subiendo
+                    --comparar posicion(entrada) y switch_ant(signal de la arquitectura)
+                    if switch_bit /= aux_sube then --si la posicion no es la que deberia ser 
+                        validez<= '0'; --hay fallo
+                    else
+                        validez <= '1'; --no hay ningun fallo
+                    end if;
+                else --sin motor es 0 (está bajando)
+                    if switch_bit /= aux_baja then
+                        validez <= '0'; --sale error
+                    else
+                        validez <= '1'; --no hay ningún fallo
+                    end if;
+                end if; 
             end if; 
-        end if; 
-        switch_ant <= switch_bit;
+            switch_ant <= switch_bit;
+        end if;    
     end process;
     
     switch_teorico:process
